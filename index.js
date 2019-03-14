de64c40f2feb3cd34d5ac468edfb4062d725a99f
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-14 10:08:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function theBeatlesPlay(musicians, instruments){
   var emptyArr = [];
   for (let i = 0; i < musicians.length; i++) {
-    array[i]
+    emptyArr.push(`${musicians[i]} plays ${instruments}.`)
   }
 }

bd8c1fd597721a2ddcaf9e23f49579850d8a51b9
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-14 10:03:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var emptyArr = [];
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < musicians.length; i++) {
     array[i]
   }
 }
